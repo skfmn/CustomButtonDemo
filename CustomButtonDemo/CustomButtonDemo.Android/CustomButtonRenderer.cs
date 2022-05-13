f6eaca91348195508fbb2475fe593a33bf2db5a3
skfmn <84275732+skfmn@users.noreply.github.com>
2022-05-13 16:41:09 -0500

    Add project files.

---
+++ CustomButtonDemo/CustomButtonDemo.Android/CustomButtonRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Android.Content;
+using CustomButtonDemo;
+using CustomButtonDemo.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomButton), typeof(CustomButtonRenderer))]
+namespace CustomButtonDemo.Droid
+{
+    public class CustomButtonRenderer : ButtonRenderer
+    {
+        public CustomButtonRenderer(Context context) : base(context)
+        { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                return;
+            }
+            var view = (CustomButton)Element;
+
+            view.Padding = new Thickness(0, 0, 0, 0);
+        }
+    }
+}
